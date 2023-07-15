2ce4fb285d103c1cea0bec0dcdd8831d48a26b2a
MynameisSejun <parkpsj0418@naver.com>
2023-07-15 23:28:33 +0900

    영화 기능 구현

--- Ticketing.cpp
+++ Ticketing.cpp
@@ -1,6 +1,22 @@
 #include "Ticketing.h"
 #include <iostream>
 
+#define ADMIN_PASSWORD 1234
+
+enum {
+	SEARCH = 1,
+	RESERVATION,
+	RESERVATION_CHECK,
+	RESERVATION_CANCEL,
+	ADMIN,
+	EXIT
+};
+enum {
+	ADD_MOVIE = 1,
+	DELETE_MOVIE,
+	SEARCH_MOVIE,
+	MAIN
+};
 Ticketing::Ticketing()
 {
 
@@ -24,6 +40,13 @@ void Ticketing::showMenu() {
 	cout << "=================================================\n";
 }
 
+int Ticketing::inputMenu() {
+	int menuNum = -1;
+	cout << "입력 : ";
+	cin >> menuNum;
+	return menuNum;
+}
+
 void Ticketing::showAdminMenu() {
 	cout << "================ 관리자 메뉴 ================\n";
 	cout << "1. 영화 추가\n";
@@ -31,4 +54,84 @@ void Ticketing::showAdminMenu() {
 	cout << "3. 영화 조회\n";
 	cout << "4. 메인으로\n";
 	cout << "============================================\n";
+}
+
+void Ticketing::searchMovie() {
+	cout << "==== 영화검색\n";
+	cout << "=================================================\n";
+
+	list<Movie>::iterator it;
+	for (it = movieList.begin(); it != movieList.end(); it++) {
+		cout << "고유번호 : " << it->getId() << "\n" << "제목 : " << it->getName() << "\n" << "장르 : " << it->getGenre() << "\n";
+		cout << endl;
+	}
+	cout << "-------------------------------------------------\n";
+}
+
+void Ticketing::deleteMovie() {
+	int tempId;
+	cout << "==== 영화삭제\n";
+	searchMovie();
+	cout << "삭제 할 영화 고유번호 : ";
+	cin >> tempId;
+
+	list<Movie>::iterator it;
+	for (it = movieList.begin(); it != movieList.end(); ++it) {
+		if (it->getId() == tempId) {
+			movieList.erase(it);
+			cout << "영화가 삭제되었습니다.\n";
+			break;
+		}
+	}
+}
+
+int Ticketing::generateId() {
+	return genId++;
+}
+
+void Ticketing::addMovie() {
+	string name;
+	string genre;
+
+	cout << "==== 영화추가\n";
+	cout << "제목 : ";
+	cin >> name;
+	cout << "장르 : ";
+	cin >> genre;
+
+	Movie movie = Movie(generateId(), name, genre, Theater());
+
+	movieList.push_back(movie);
+}
+
+void Ticketing::changeAdmin() {
+	int pw = 0;
+	cout << "관리자 암호 입력 : ";
+	cin >> pw;
+
+	if (pw != ADMIN_PASSWORD) {
+		cout << "패스워드가 일치하지 않습니다.\n";
+		initMenu();
+	}
+
+	while (true) {
+		showAdminMenu();
+		int menuNum = inputMenu();
+		switch (menuNum) {
+		case ADD_MOVIE:
+			addMovie();
+			break;
+		case DELETE_MOVIE:
+			deleteMovie();
+			break;
+		case SEARCH_MOVIE:
+			searchMovie();
+			break;
+		case MAIN:
+			initMenu();
+			break;
+		default:
+			break;
+		}
+	}
 }
--- Ticketing.h
+++ Ticketing.h
@@ -3,15 +3,24 @@
 #include "Theater.h"
 #include "Movie.h"
 #include <iostream>
+#include <list>
 using namespace std;
 
 class Ticketing {
+	int genId = 1000; // +
+	list<Movie> movieList; // +
 public:
 	Ticketing();
 	~Ticketing();
-	void showMenu();
-	void showAdminMenu();
-	void showTitle();
+	int generateId(); // 
+	void showMenu(); //
+	void showAdminMenu(); //
+	void showTitle(); //
+	int inputMenu(); // 메뉴 입력 +
+	void searchMovie(); //영화 검색 +
+	void deleteMovie(); // 영화 삭제 +
+	void changeAdmin(); // 관리자 모드 +
+	void addMovie(); // 영화 추가 +
 };
 
 #endif
